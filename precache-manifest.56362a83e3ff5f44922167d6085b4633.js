d6b1b48325c46ee3f134773f5093c8e5eb6c0b25
szkapec <szkapec999@o2.pl>
2020-09-29 17:55:57 +0200

    Updates

---
+++ precache-manifest.56362a83e3ff5f44922167d6085b4633.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc97bd63f91b53cb49764cfb714af11",
+    "url": "/appbaflix/index.html"
+  },
+  {
+    "revision": "26a1db9d9460a7e472fa",
+    "url": "/appbaflix/static/css/main.f1ada8e3.chunk.css"
+  },
+  {
+    "revision": "82ca1affb39fda3acdfb",
+    "url": "/appbaflix/static/js/2.1a25af5e.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/appbaflix/static/js/2.1a25af5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26a1db9d9460a7e472fa",
+    "url": "/appbaflix/static/js/main.aced535e.chunk.js"
+  },
+  {
+    "revision": "d938883403c31557f378",
+    "url": "/appbaflix/static/js/runtime-main.7750c8e9.js"
+  },
+  {
+    "revision": "edaf83e97449ca534d605db5abef597b",
+    "url": "/appbaflix/static/media/640.edaf83e9.jpg"
+  },
+  {
+    "revision": "55e00c41da66d06f4d774ab3903ac272",
+    "url": "/appbaflix/static/media/Glosss.55e00c41.m4a"
+  },
+  {
+    "revision": "fab25bc378670766a10e1ec37712945e",
+    "url": "/appbaflix/static/media/tab-1-pic.fab25bc3.png"
+  },
+  {
+    "revision": "66c7c1fb5b71b2deaff929bbcd3bacf7",
+    "url": "/appbaflix/static/media/tab-macbook.66c7c1fb.png"
+  },
+  {
+    "revision": "dc986d819c053deb6180a34320af2988",
+    "url": "/appbaflix/static/media/tab-tablet.dc986d81.png"
+  },
+  {
+    "revision": "9f91203b7c374a590dd727c323d5b6dc",
+    "url": "/appbaflix/static/media/tab-tv.9f91203b.png"
+  }
+]);
